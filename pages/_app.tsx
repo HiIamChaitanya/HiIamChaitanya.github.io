f10d2ecb085ae864d95d86aede08caf8623f6647
Chaitanya <73682988+HiIamChaitanya@users.noreply.github.com>
2022-07-20 06:13:13 +0530

    main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,23 +3,6 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import MetaContainer from '@components/MetaContainer'
 import Head from 'next/head'
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBmoZ2gX2MD5x_X4XiYwzwSs-mM3FGCONc",
-  authDomain: "hiiamchaitanya-efb98.firebaseapp.com",
-  projectId: "hiiamchaitanya-efb98",
-  storageBucket: "hiiamchaitanya-efb98.appspot.com",
-  messagingSenderId: "187738907107",
-  appId: "1:187738907107:web:cf9b4859ebb86d4f10c0d6",
-  measurementId: "G-FHH1NGK5WK"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 
 function MyApp({ Component, pageProps }: AppProps) {
